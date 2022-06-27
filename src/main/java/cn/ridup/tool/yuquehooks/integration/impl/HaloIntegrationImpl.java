ca5676bf5f60f68e05c21a66d169064df5c1454f
Ridup <mu_ran@yahoo.com>
2022-06-27 08:56:16 +0800

    feat(halo): halo content optimize

--- src/main/java/cn/ridup/tool/yuquehooks/integration/impl/HaloIntegrationImpl.java
+++ src/main/java/cn/ridup/tool/yuquehooks/integration/impl/HaloIntegrationImpl.java
@@ -9,6 +9,7 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
@@ -18,8 +19,8 @@ import org.springframework.web.client.RestTemplate;
 import cn.ridup.tool.yuquehooks.cache.AbstractStringCacheStore;
 import cn.ridup.tool.yuquehooks.config.properties.YuqueProperties;
 import cn.ridup.tool.yuquehooks.integration.HaloIntegration;
-import cn.ridup.tool.yuquehooks.integration.request.HaloPostRequestDto;
 import cn.ridup.tool.yuquehooks.integration.request.HaloLoginRequestDto;
+import cn.ridup.tool.yuquehooks.integration.request.HaloPostRequestDto;
 import cn.ridup.tool.yuquehooks.integration.request.PostStatus;
 import cn.ridup.tool.yuquehooks.integration.request.QueryPostListRequestDto;
 import cn.ridup.tool.yuquehooks.integration.response.HaloCommonDto;
@@ -56,7 +57,6 @@ public class HaloIntegrationImpl implements HaloIntegration {
 
     private static final String HALO_REQUEST_HEADER_TOKEN_KEY = "ADMIN-Authorization";
 
-
     /** <a href="https://api.halo.run/admin-api.html#operation/authUsingPOST">Creates a post</a> */
     private static final String HALO_LOGIN = "/api/admin/login";
 
@@ -74,47 +74,62 @@ public class HaloIntegrationImpl implements HaloIntegration {
 
     public HaloIntegrationImpl(AbstractStringCacheStore cacheStore) {this.cacheStore = cacheStore;}
 
-    public <E, T> HaloCommonDto<T> fetchForEntityWithHeader(String path,HttpMethod method,E request,
+    public <E, T> HaloCommonDto<T> fetchForEntityWithHeader(String path, HttpMethod method, E request,
         ParameterizedTypeReference<HaloCommonDto<T>> type) {
-        Assert.notNull(yuqueProperties.getHalo().getHost() , "halo host is null");
-        String url = yuqueProperties.getHalo().getHost() + path;
+        Assert.notNull(yuqueProperties.getHalo()
+            .getHost(), "halo host is null");
+        String url = yuqueProperties.getHalo()
+            .getHost() + path;
         HttpEntity<E> httpEntity = new HttpEntity<>(request, getDefaultHttpHeader(!HALO_LOGIN.equals(path)));
-        ResponseEntity<HaloCommonDto<T>> haloCommonDtoResponseEntity = restTemplate.exchange(url, method,
-            httpEntity, type);
+        ResponseEntity<HaloCommonDto<T>> haloCommonDtoResponseEntity = restTemplate.exchange(url, method, httpEntity,
+            type);
+        if (null != haloCommonDtoResponseEntity.getBody() && haloCommonDtoResponseEntity.getBody()
+            .getStatus()
+            .equals(HttpStatus.UNAUTHORIZED.value())) {
+            login(yuqueProperties.getHalo()
+                .getUsername(), yuqueProperties.getHalo()
+                .getPassword());
+        }
         return haloCommonDtoResponseEntity.getBody();
     }
 
-    public <E, T> HaloCommonDto<T> postForEntityWithHeader(String path,E request,
+    public <E, T> HaloCommonDto<T> postForEntityWithHeader(String path, E request,
         ParameterizedTypeReference<HaloCommonDto<T>> type) {
-        return fetchForEntityWithHeader(path,HttpMethod.POST,request,type);
+        return fetchForEntityWithHeader(path, HttpMethod.POST, request, type);
     }
 
-    public <E, T> HaloCommonDto<T> putForEntityWithHeader(String path,E request,
+    public <E, T> HaloCommonDto<T> putForEntityWithHeader(String path, E request,
         ParameterizedTypeReference<HaloCommonDto<T>> type) {
-        return fetchForEntityWithHeader(path,HttpMethod.PUT,request,type);
+        return fetchForEntityWithHeader(path, HttpMethod.PUT, request, type);
     }
 
-    public <E, T> HaloCommonDto<T> getForEntityWithHeader(String path,E request,
+    public <E, T> HaloCommonDto<T> getForEntityWithHeader(String path, E request,
         ParameterizedTypeReference<HaloCommonDto<T>> type) {
-        return fetchForEntityWithHeader(path,HttpMethod.GET,request,type);
+        return fetchForEntityWithHeader(path, HttpMethod.GET, request, type);
     }
 
     /**
      * 获取默认的请求头
+     *
      * @param tokenCheck 是否需要检查token
      * @return HttpHeaders 请求头
      */
     public HttpHeaders getDefaultHttpHeader(boolean tokenCheck) {
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_UTF8_VALUE);
-        if(tokenCheck){
-            if(cacheStore.get(HALO_ADMIN_TOKEN_CACHE_KEY)
+        if (tokenCheck) {
+            if (cacheStore.get(HALO_ADMIN_TOKEN_CACHE_KEY)
                 .isEmpty()) {
-                Assert.notNull(yuqueProperties.getHalo().getUsername() , "halo  username is null");
-                Assert.notNull(yuqueProperties.getHalo().getPassword() , "halo  password is null");
-                login(yuqueProperties.getHalo().getUsername(),yuqueProperties.getHalo().getPassword());
+                Assert.notNull(yuqueProperties.getHalo()
+                    .getUsername(), "halo  username is null");
+                Assert.notNull(yuqueProperties.getHalo()
+                    .getPassword(), "halo  password is null");
+                login(yuqueProperties.getHalo()
+                    .getUsername(), yuqueProperties.getHalo()
+                    .getPassword());
             }
-            headers.add(HALO_REQUEST_HEADER_TOKEN_KEY, cacheStore.get(HALO_ADMIN_TOKEN_CACHE_KEY).get());
+            headers.add(HALO_REQUEST_HEADER_TOKEN_KEY, cacheStore.get(HALO_ADMIN_TOKEN_CACHE_KEY)
+                .get());
         }
         return headers;
     }
@@ -124,8 +139,8 @@ public class HaloIntegrationImpl implements HaloIntegration {
         HaloLoginRequestDto haloLoginRequestDto = new HaloLoginRequestDto();
         haloLoginRequestDto.setUsername(username);
         haloLoginRequestDto.setPassword(password);
-        HaloCommonDto<HaloLoginResponseDto> haloLoginResponseDtoHaloCommonDto = postForEntityWithHeader(
-            HALO_LOGIN,haloLoginRequestDto, new ParameterizedTypeReference<HaloCommonDto<HaloLoginResponseDto>>() {
+        HaloCommonDto<HaloLoginResponseDto> haloLoginResponseDtoHaloCommonDto = postForEntityWithHeader(HALO_LOGIN,
+            haloLoginRequestDto, new ParameterizedTypeReference<HaloCommonDto<HaloLoginResponseDto>>() {
             });
         HaloLoginResponseDto data = haloLoginResponseDtoHaloCommonDto.getData();
         cacheStore.put(HALO_ADMIN_TOKEN_CACHE_KEY, data.getAccessToken(), data.getExpiredIn(), TimeUnit.SECONDS);
@@ -134,37 +149,38 @@ public class HaloIntegrationImpl implements HaloIntegration {
 
     @Override
     public HaloPostResponseDto createPost(HaloPostRequestDto requestDto) {
-        HaloCommonDto<HaloPostResponseDto> createPostResponseDto = postForEntityWithHeader(
-            CREATES_POST,requestDto.getPostParam(), new ParameterizedTypeReference<HaloCommonDto<HaloPostResponseDto>>() {
+        HaloCommonDto<HaloPostResponseDto> createPostResponseDto = postForEntityWithHeader(CREATES_POST,
+            requestDto.getPostParam(), new ParameterizedTypeReference<HaloCommonDto<HaloPostResponseDto>>() {
             });
         return createPostResponseDto.getData();
     }
 
     @Override
-    public HaloPostResponseDto updatePost(HaloPostRequestDto requestDto,Integer postId) {
-        HaloCommonDto<HaloPostResponseDto> updatePostResponseDto = putForEntityWithHeader(
-            UPDATES_POST+postId,requestDto.getPostParam(), new ParameterizedTypeReference<HaloCommonDto<HaloPostResponseDto>>() {
+    public HaloPostResponseDto updatePost(HaloPostRequestDto requestDto, Integer postId) {
+        HaloCommonDto<HaloPostResponseDto> updatePostResponseDto = putForEntityWithHeader(UPDATES_POST + postId,
+            requestDto.getPostParam(), new ParameterizedTypeReference<HaloCommonDto<HaloPostResponseDto>>() {
             });
         return updatePostResponseDto.getData();
     }
 
     @Override
-    public Page<BasePostSimpleDTO> queryPostList(String title,int page,int size) {
+    public Page<BasePostSimpleDTO> queryPostList(String title, int page, int size) {
         QueryPostListRequestDto requestDto = new QueryPostListRequestDto();
         requestDto.setKeyword(title);
         requestDto.setPage(page);
         requestDto.setSize(size);
-        HaloCommonDto<Page<BasePostSimpleDTO>> createPostResponseDto = getForEntityWithHeader(
-            LISTS_POSTS,requestDto, new ParameterizedTypeReference<HaloCommonDto<Page<BasePostSimpleDTO>>>() {
+        HaloCommonDto<Page<BasePostSimpleDTO>> createPostResponseDto = getForEntityWithHeader(LISTS_POSTS+"?size="+size+"&page="+page, requestDto,
+            new ParameterizedTypeReference<HaloCommonDto<Page<BasePostSimpleDTO>>>() {
             });
         return createPostResponseDto.getData();
     }
 
     @Override
     public BasePostSimpleDTO updateStatusBy(Integer postId, PostStatus status) {
-        Assert.notNull(postId,"updateStatusBy's postId is null");
+        Assert.notNull(postId, "updateStatusBy's postId is null");
         HaloCommonDto<BasePostSimpleDTO> updatePostResponseDto = putForEntityWithHeader(
-            UPDATES_POST_STATUS+postId+"/status/"+status.toString(),null, new ParameterizedTypeReference<HaloCommonDto<BasePostSimpleDTO>>() {
+            UPDATES_POST_STATUS + postId + "/status/" + status.toString(), null,
+            new ParameterizedTypeReference<HaloCommonDto<BasePostSimpleDTO>>() {
             });
         return updatePostResponseDto.getData();
     }
--- src/main/java/cn/ridup/tool/yuquehooks/service/convertor/HaloDataConvertor.java
+++ src/main/java/cn/ridup/tool/yuquehooks/service/convertor/HaloDataConvertor.java
@@ -131,6 +131,31 @@ public class HaloDataConvertor {
                     element.insertChildren(0, input);
                 });
 
+            // for the image add a gallery
+            document.getElementsByClass("ne-p")
+                .forEach(element -> {
+                    if (element.childNode(0)
+                        .nodeName()
+                        .equals("img")) {
+                        String src = element.childNode(0)
+                            .attr("src");
+                        Element gallery = new Element("span");
+                        gallery.attr("data-fancybox", "gallery");
+                        gallery.attr("href", src);
+                        gallery.attr("style", "display: block;");
+                        element.childNode(0)
+                            .attr("class", "lazyload");
+                        element.childNode(0)
+                            .attr("data-src", src);
+                        element.childNode(0)
+                            .attr("data-loaded", "true");
+                        gallery.insertChildren(0, element.childNode(0).clone());
+                        element.childNode(0)
+                            .remove();
+                        element.insertChildren(0, gallery);
+                    }
+                });
+
             postParam.setContent(document.body()
                 .html());
 
@@ -143,8 +168,10 @@ public class HaloDataConvertor {
             }
 
             Elements imgList = document.getElementsByTag("img");
-            if(!imgList.isEmpty()){
-                postParam.setThumbnail(imgList.get(0).attr("src"));
+            if (!imgList.isEmpty()) {
+                // thumbnail
+                postParam.setThumbnail(imgList.get(0)
+                    .attr("src"));
             }
 
         }
